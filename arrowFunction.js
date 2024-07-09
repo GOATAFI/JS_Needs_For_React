225bd2f0d2d100e0d838780c8c1b14eb24140fd2
GOATAFI <projectexecutiveofpm@gmail.com>
2024-07-10 00:08:07 +0600

    arrow function

---
+++ arrowFunction.js
@@ -0,0 +1,22 @@
+//normal function
+function doSomething() {}
+
+//arrow function
+const doSomething = () => {};
+
+//normal function k export korte chaile export default function
+
+export default function doSomething() {}
+
+//arrow function k export korte chaile shudu export
+
+export const doSomething = () => {};
+
+//React er structure arrow function er sathe miley jay
+const MyComponent = () => {
+  return <div> </div>;
+};
+// ^^ uporer eta React er structure
+
+//Anonymous function
+<button onClick={() => {console.log("hello world")}}></button> 
